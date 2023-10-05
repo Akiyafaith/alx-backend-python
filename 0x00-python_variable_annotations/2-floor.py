298f62b1976449c7874fc3934f6a9ff84866745d
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-06 00:30:36 +0300

    floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -2,6 +2,7 @@
 """the function floor that takes a float Return the floor of a float"""
 import math
 
+
 def floor(n: float) -> int:
     """Return the floor of 'n' as an integer"""
     return math.floor(n)
