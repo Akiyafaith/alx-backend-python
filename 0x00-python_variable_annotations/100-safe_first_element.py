bf627322e6f021b7c8922f6c7ad4689b5278aece
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-06 01:56:01 +0300

    safe element

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,7 +3,7 @@
 from typing import Any, Union
 
 
-def safe_first_element(lst) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """return The first element of the list"""
     if lst:
         return lst[0]
