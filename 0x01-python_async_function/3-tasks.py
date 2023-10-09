f81b6a268cfcd56d2eb5852e4aa07302baa77f1a
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-09 21:33:32 +0300

    async

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -10,7 +10,7 @@ module = importlib.import_module(MODULE_NAME)
 wait_random = getattr(module, 'wait_random')
 
 
-async def test(max_delay: int) -> float:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """a function (do not create an async function,
     use the regular function syntax to do this)task_wait_random
     that takes an integer max_delay and returns a asyncio.Task"""
